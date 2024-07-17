87820541d6158e6eeda88ac334a741dd5f5d71f1
GitHub Action <action@github.com>
2024-07-17 05:28:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/bfinecpa/testtoexcel/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/bfinecpa/testtoexcel/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.bfinecpa.testtoexcel.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.bfinecpa.testtoexcel.MyBundle
+import com.github.bfinecpa.testtoexcel.services.MyProjectService
 import javax.swing.JButton
 
 
